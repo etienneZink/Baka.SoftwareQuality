6336acf293f6711652a86442f9ba28e584954fb8
Etienne Zink <etienne-zink@gmx.de>
2022-04-30 11:04:15 +0200

    Added dependency injection for app and services as their interface.

--- src/Baka.ContactSplitter/App.xaml.cs
+++ src/Baka.ContactSplitter/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using Autofac;
 
 namespace Baka.ContactSplitter
 {
@@ -13,5 +15,17 @@ namespace Baka.ContactSplitter
     /// </summary>
     public partial class App : Application
     {
+        public IContainer Container { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ContainerBuilder containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(t => t.Namespace.Contains("services") && t.IsClass)
+                .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name == "I" + t.Name));
+            containerBuilder.RegisterInstance(this);
+
+            Container = containerBuilder.Build();
+        }
     }
 }
