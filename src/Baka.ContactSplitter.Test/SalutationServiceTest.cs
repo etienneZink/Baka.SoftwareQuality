1ddedc9de1bc82a30dd2ca18c76062764af5ea8e
Etienne Zink <etienne-zink@gmx.de>
2022-05-05 10:00:12 +0200

    Added Tests for Salutation- and TitleService.

---
+++ src/Baka.ContactSplitter.Test/SalutationServiceTest.cs
@@ -0,0 +1,110 @@
+﻿using System.Linq;
+using Baka.ContactSplitter.Model;
+using Baka.ContactSplitter.Services.Implementations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Baka.ContactSplitter.Test
+{
+    [TestClass]
+    public class SalutationServiceTest
+    {
+        [TestMethod]
+        [DataRow("Frau")]
+        [DataRow("Mrs.")]
+        [DataRow("Ms.")]
+        [DataRow("Mmd.")]
+        [DataRow("Herr")]
+        [DataRow("Mr.")]
+        [DataRow("Sir")]
+        public void SalutationService_GetSalutations_ContainsSalutation(string salutation)
+        {
+            // Arrange
+            var salutationService = new SalutationService();
+
+            // Act
+            var salutations = salutationService.GetSalutations();
+
+            // Assert
+            Assert.IsTrue(salutations.Contains(salutation));
+        }
+
+        [TestMethod]
+        [DataRow("Test Anrede")]
+        [DataRow("Frau Madame")]
+        public void SalutationService_GetSalutations_NotContainsSalutation(string salutation)
+        {
+            // Arrange
+            var salutationService = new SalutationService();
+
+            // Act
+            var salutations = salutationService.GetSalutations();
+
+            // Assert
+            Assert.IsFalse(salutations.Contains(salutation));
+        }
+
+        [TestMethod]
+        [DataRow("Frau", Gender.Female)]
+        [DataRow("Mrs.", Gender.Female)]
+        [DataRow("Ms.", Gender.Female)]
+        [DataRow("Mmd.", Gender.Female)]
+        [DataRow("Herr", Gender.Male)]
+        [DataRow("Mr.", Gender.Male)]
+        [DataRow("Sir", Gender.Male)]
+        public void SalutationService_GetGender_Success(string salutation, Gender expectedGender)
+        {
+            // Arrange
+            var salutationService = new SalutationService();
+
+            // Act
+            var gender = salutationService.GetGender(salutation);
+
+            // Assert
+            Assert.AreEqual(expectedGender, gender);
+        }
+
+        [TestMethod]
+        [DataRow("Test Anrede")]
+        [DataRow("Frau Madame")]
+        public void SalutationService_GetGender_FalseTitle(string salutation)
+        {
+            // Arrange
+            var salutationService = new SalutationService();
+
+            // Act
+            var gender = salutationService.GetGender(salutation);
+
+            // Assert
+            Assert.AreEqual(Gender.Neutral, gender);
+        }
+
+        // Test needs to test add and delete because otherwise it could be influencing other tests!
+        [TestMethod]
+        [DataRow("Test Anrede", Gender.Neutral)]
+        [DataRow("Frau Madame", Gender.Female)]
+        public void SalutationService_SaveAndDeleteSalutation_Success(string salutation, Gender gender)
+        {
+            // Arrange
+            var salutationService = new SalutationService();
+
+            // Act
+            // Save
+            var saved = salutationService.SaveOrUpdateSalutation(salutation, gender);
+            var savedSalutations = salutationService.GetSalutations().ToArray();
+            var savedGender = salutationService.GetGender(salutation);
+            //Delete
+            var deleted = salutationService.DeleteSalutation(salutation);
+            var deletedSalutation = salutationService.GetSalutations().ToArray();
+            var deletedGender = salutationService.GetGender(salutation);
+
+            // Assert
+            Assert.IsTrue(saved);
+            Assert.IsTrue(savedSalutations.Contains(salutation));
+            Assert.AreEqual(gender, savedGender);
+
+            Assert.IsTrue(deleted);
+            Assert.IsFalse(deletedSalutation.Contains(salutation));
+            Assert.AreEqual(Gender.Neutral, deletedGender);
+        }
+    }
+}
---
+++ src/Baka.ContactSplitter.Test/TitleServiceTests.cs
@@ -0,0 +1,111 @@
+﻿using System.Linq;
+using Baka.ContactSplitter.Services.Implementations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Baka.ContactSplitter.Test
+{
+    [TestClass]
+    public class TitleServiceTests
+    {
+        [TestMethod]
+        [DataRow("Professor")]
+        [DataRow("Professorin")]
+        [DataRow("Prof.")]
+        [DataRow("Dr.")]
+        [DataRow("Dr. rer. nat.")]
+        [DataRow("Dipl.-Ing.")]
+        [DataRow("M.Sc.")]
+        [DataRow("B.Sc.")]
+        public void TitleService_GetTitles_ContainsTitle(string title)
+        {
+            // Arrange
+            var titleService = new TitleService();
+            
+            // Act
+            var titles = titleService.GetTitles();
+
+            // Assert
+            Assert.IsTrue(titles.Contains(title));
+        }
+
+        [TestMethod]
+        [DataRow("B.A.")]
+        [DataRow("M.A.")]
+        public void TitleService_GetTitles_NotContainsTitle(string title)
+        {
+            // Arrange
+            var titleService = new TitleService();
+
+            // Act
+            var titles = titleService.GetTitles();
+
+            // Assert
+            Assert.IsFalse(titles.Contains(title));
+        }
+
+        [TestMethod]
+        [DataRow("Professor", "Prof.")]
+        [DataRow("Professorin", "Prof.")]
+        [DataRow("Prof.", "Prof.")]
+        [DataRow("Dr.", "Dr.")]
+        [DataRow("Dr. rer. nat.", "Dr.")]
+        [DataRow("Dipl.-Ing.", "Dipl.-Ing.")]
+        [DataRow("M.Sc.", "")]
+        [DataRow("B.Sc.", "")]
+        public void TitleService_GetTitleSalutation_Success(string title, string expectedTitleSalutation)
+        {
+            // Arrange
+            var titleService = new TitleService();
+
+            // Act
+            var titleSalutation = titleService.GetTitleSalutation(title);
+
+            // Assert
+            Assert.AreEqual(expectedTitleSalutation, titleSalutation);
+        }
+
+        [TestMethod]
+        [DataRow("B.A.")]
+        [DataRow("M.A.")]
+        public void TitleService_GetTitleSalutation_FalseTitle(string title)
+        {
+            // Arrange
+            var titleService = new TitleService();
+
+            // Act
+            var titleSalutation = titleService.GetTitleSalutation(title);
+
+            // Assert
+            Assert.AreEqual(string.Empty, titleSalutation);
+        }
+
+        // Test needs to test add and delete because otherwise it could be influencing other tests!
+        [TestMethod]
+        [DataRow("B.A.", "B.A.")]
+        [DataRow("M.A.", "M.A.")]
+        public void TitleService_SaveAndDeleteTitle_Success(string title, string titleSalutation)
+        {
+            // Arrange
+            var titleService = new TitleService();
+
+            // Act
+            // Save
+            var saved = titleService.SaveOrUpdateTitle(title, titleSalutation);
+            var savedTitles = titleService.GetTitles().ToArray();
+            var savedTitleSalutation = titleService.GetTitleSalutation(title);
+            //Delete
+            var deleted = titleService.DeleteTitle(title);
+            var deletedTitles = titleService.GetTitles().ToArray();
+            var deletedTitleSalutation = titleService.GetTitleSalutation(title);
+
+            // Assert
+            Assert.IsTrue(saved);
+            Assert.IsTrue(savedTitles.Contains(title));
+            Assert.AreEqual(titleSalutation, savedTitleSalutation);
+
+            Assert.IsTrue(deleted);
+            Assert.IsFalse(deletedTitles.Contains(title));
+            Assert.AreEqual(string.Empty, deletedTitleSalutation);
+        }
+    }
+}
